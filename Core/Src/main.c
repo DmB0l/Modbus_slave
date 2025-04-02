fc2b56f74cc0b6243dd3965d8906aebac7345f9f
DmB0l <113234018+DmB0l@users.noreply.github.com>
2025-04-02 13:52:50 +0300

    work
    Работает с моей либой для модбас. Перегружен вывод printf в uart. Сделан нормальный вывод в консоль.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "modbus.h"
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -64,7 +65,9 @@ static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
-void send_message(void);
+void Send_Message(void);
+void PrintHexArray8(const uint8_t *array, int length);
+void PrintHexArray16(const uint16_t *array, int length);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -77,61 +80,61 @@ void send_message(void);
  * @retval int
  */
 int main(void) {
-    /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-    /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-    /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-    HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-    /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-    /* USER CODE END Init */
+	/* USER CODE END Init */
 
-    /* Configure the system clock */
-    SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-    /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-    /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
-    MX_USART1_UART_Init();
-    MX_USART2_UART_Init();
-    /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART1_UART_Init();
+	MX_USART2_UART_Init();
+	/* USER CODE BEGIN 2 */
 
-    start_time = HAL_GetTick();
-    device = modbus_init_device(100, 100, 100, 100); // Инициализация устройства Modbus
+	start_time = HAL_GetTick();
+	device = modbus_init_device(100, 100, 100, 100); // Инициализация устройства Modbus
 
-    HAL_UART_Receive_IT(&huart1, rx_buffer, 8);      // Начинаем прием запросов
-    HAL_GPIO_WritePin(GPIOA, DREnabled_Pin, GPIO_PIN_RESET);
+	HAL_UART_Receive_IT(&huart1, rx_buffer, 8);      // Начинаем прием запросов
+	HAL_GPIO_WritePin(GPIOA, DREnabled_Pin, GPIO_PIN_RESET);
 
-    /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-    /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
-    while (1) {
-        /* USER CODE END WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
+		/* USER CODE END WHILE */
 
-        /* USER CODE BEGIN 3 */
-        if (send_flag) {
-            send_message(); // Отправляем ответ, если флаг установлен
-            send_flag = 0;
-        }
+		/* USER CODE BEGIN 3 */
+//		if (send_flag) {
+//			Send_Message(); // Отправляем ответ, если флаг установлен
+//			send_flag = 0;
+//		}
 
-        HAL_GPIO_TogglePin(GPIOC, LED_Pin); // Мигаем светодиодом для индикации работы
-        HAL_Delay(100);
+		HAL_GPIO_TogglePin(GPIOC, LED_Pin); // Мигаем светодиодом для индикации работы
+		HAL_Delay(100);
 
-        if(HAL_GetTick() - start_time > 5000) {
-        	HAL_UART_AbortReceive(&huart1);  // Аварийная остановка приема (если он был запущен)
-        	HAL_UART_Receive_IT(&huart1, rx_buffer, 8);      // Начинаем прием запросов
-        	start_time = HAL_GetTick();
-        }
-    }
-    /* USER CODE END 3 */
+		if (HAL_GetTick() - start_time > 5000) {
+			HAL_UART_AbortReceive(&huart1);  // Аварийная остановка приема (если он был запущен)
+			HAL_UART_Receive_IT(&huart1, rx_buffer, 8);      // Начинаем прием запросов
+			start_time = HAL_GetTick();
+		}
+	}
+	/* USER CODE END 3 */
 }
 
 /**
@@ -139,29 +142,29 @@ int main(void) {
  * @retval None
  */
 void SystemClock_Config(void) {
-    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        Error_Handler();
-    }
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
-        Error_Handler();
-    }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
@@ -170,17 +173,17 @@ void SystemClock_Config(void) {
  * @retval None
  */
 static void MX_USART1_UART_Init(void) {
-    huart1.Instance = USART1;
-    huart1.Init.BaudRate = 9600;
-    huart1.Init.WordLength = UART_WORDLENGTH_8B;
-    huart1.Init.StopBits = UART_STOPBITS_1;
-    huart1.Init.Parity = UART_PARITY_NONE;
-    huart1.Init.Mode = UART_MODE_TX_RX;
-    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-    if (HAL_UART_Init(&huart1) != HAL_OK) {
-        Error_Handler();
-    }
+	huart1.Instance = USART1;
+	huart1.Init.BaudRate = 9600;
+	huart1.Init.WordLength = UART_WORDLENGTH_8B;
+	huart1.Init.StopBits = UART_STOPBITS_1;
+	huart1.Init.Parity = UART_PARITY_NONE;
+	huart1.Init.Mode = UART_MODE_TX_RX;
+	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+	if (HAL_UART_Init(&huart1) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
@@ -189,17 +192,17 @@ static void MX_USART1_UART_Init(void) {
  * @retval None
  */
 static void MX_USART2_UART_Init(void) {
-    huart2.Instance = USART2;
-    huart2.Init.BaudRate = 9600;
-    huart2.Init.WordLength = UART_WORDLENGTH_8B;
-    huart2.Init.StopBits = UART_STOPBITS_1;
-    huart2.Init.Parity = UART_PARITY_NONE;
-    huart2.Init.Mode = UART_MODE_TX_RX;
-    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-    if (HAL_UART_Init(&huart2) != HAL_OK) {
-        Error_Handler();
-    }
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 9600;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	if (HAL_UART_Init(&huart2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
@@ -208,68 +211,93 @@ static void MX_USART2_UART_Init(void) {
  * @retval None
  */
 static void MX_GPIO_Init(void) {
-    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOD_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
 
-    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(DREnabled_GPIO_Port, DREnabled_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DREnabled_GPIO_Port, DREnabled_Pin, GPIO_PIN_RESET);
 
-    GPIO_InitStruct.Pin = LED_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+	GPIO_InitStruct.Pin = LED_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = DREnabled_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(DREnabled_GPIO_Port, &GPIO_InitStruct);
+	GPIO_InitStruct.Pin = DREnabled_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(DREnabled_GPIO_Port, &GPIO_InitStruct);
 }
 
 /* USER CODE BEGIN 4 */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-    if (huart == &huart1) {
-        start_time = HAL_GetTick();
-        if (func_code == 0) {
-            func_code = rx_buffer[1];
-            if (func_code == 0x10 || func_code == 0x0F) {
-                uint8_t ost_size = rx_buffer[6] - 1 + 2; // Остаток данных для функций 0x10 и 0x0F
-                rx_real_size = 8 + ost_size;
-                if (rx_real_size > sizeof(rx_buffer)) {
-                    func_code = 0; // Ошибка: буфер слишком мал
-                    rx_real_size = 0;
-                    HAL_UART_Receive_IT(&huart1, rx_buffer, 8);
-                    return;
-                }
-                HAL_UART_Receive_IT(&huart1, &rx_buffer[8], ost_size);
-            } else {
-                func_code = 0;
-                modbus_process_response(device, rx_buffer, 8, response_buffer, &resp_length);
-                send_flag = 1; // Устанавливаем флаг для отправки ответа
-                HAL_UART_Transmit(&huart2, response_buffer, resp_length, 100); // Отладка через huart2
-                HAL_UART_Receive_IT(&huart1, rx_buffer, 8); // Готовимся к следующему запросу
-            }
-        } else {
-            func_code = 0;
-            modbus_process_response(device, rx_buffer, rx_real_size, response_buffer, &resp_length);
-            send_flag = 1; // Устанавливаем флаг для отправки ответа
-            HAL_UART_Transmit(&huart2, response_buffer, resp_length, 100); // Отладка через huart2
-            rx_real_size = 0;
-            HAL_UART_Receive_IT(&huart1, rx_buffer, 8); // Готовимся к следующему запросу
-        }
-    }
+	if (huart == &huart1) {
+		start_time = HAL_GetTick();
+		if (func_code == 0) {
+			func_code = rx_buffer[1];
+			if (func_code == 0x10 || func_code == 0x0F) {
+				uint8_t ost_size = rx_buffer[6] - 1 + 2; // Остаток данных для функций 0x10 и 0x0F
+				rx_real_size = 8 + ost_size;
+				if (rx_real_size > sizeof(rx_buffer)) {
+					func_code = 0; // Ошибка: буфер слишком мал
+					rx_real_size = 0;
+					HAL_UART_Receive_IT(&huart1, rx_buffer, 8);
+					return;
+				}
+				HAL_UART_Receive_IT(&huart1, &rx_buffer[8], ost_size);
+			} else {
+				func_code = 0;
+				rx_real_size = 0;
+				modbus_process_response_from_master(device, rx_buffer, 8, response_buffer, &resp_length);
+				printf("Receiving request from master: ");
+				PrintHexArray8(rx_buffer, 8);
+				Send_Message();
+				HAL_UART_Receive_IT(&huart1, rx_buffer, 8); // Готовимся к следующему запросу
+			}
+		} else {
+			func_code = 0;
+			modbus_process_response_from_master(device, rx_buffer, rx_real_size, response_buffer, &resp_length);
+			printf("Receiving request from master: ");
+			PrintHexArray8(rx_buffer, rx_real_size);
+			Send_Message();
+			rx_real_size = 0;
+			HAL_UART_Receive_IT(&huart1, rx_buffer, 8); // Готовимся к следующему запросу
+		}
+	}
 }
 
-void send_message(void) {
+void Send_Message(void) {
 //    if (HAL_UART_GetState(&huart1) == HAL_UART_STATE_READY) {
-        HAL_GPIO_WritePin(GPIOA, DREnabled_Pin, GPIO_PIN_SET); // Переключаем в режим передачи
-        HAL_UART_Transmit(&huart1, response_buffer, resp_length, 100); // Асинхронная отправка
-        HAL_GPIO_WritePin(GPIOA, DREnabled_Pin, GPIO_PIN_RESET); // Переключаем в режим передачи
+	printf("Sending answer to master: ");
+	PrintHexArray8(response_buffer, resp_length);
+	HAL_GPIO_WritePin(GPIOA, DREnabled_Pin, GPIO_PIN_SET); // Переключаем в режим передачи
+	HAL_UART_Transmit(&huart1, response_buffer, resp_length, 100); // Асинхронная отправка
+	HAL_GPIO_WritePin(GPIOA, DREnabled_Pin, GPIO_PIN_RESET); // Переключаем в режим передачи
 //    }
+}
+
+void PrintHexArray8(const uint8_t *array, int length) {
+    for (int i = 0; i < length; i++) {
+        printf("%02X ", array[i]); // %02X для HEX в верхнем регистре
+    }
+    printf("\n");
+}
+
+void PrintHexArray16(const uint16_t *array, int length) {
+	for (uint16_t i = 0; i < length; i++) {
+	    printf("%04X ", array[i]); // Вывод uint16_t в HEX
+	}
+	printf("\n");
+}
+
+// Перенаправление printf в UART2
+int _write(int file, uint8_t *ptr, int len) {  // Лучше использовать uint8_t*
+    HAL_UART_Transmit(&huart2, ptr, len, HAL_MAX_DELAY);
+    return len;
 }
 
 /* USER CODE END 4 */
@@ -279,9 +307,9 @@ void send_message(void) {
  * @retval None
  */
 void Error_Handler(void) {
-    __disable_irq();
-    while (1) {
-    }
+	__disable_irq();
+	while (1) {
+	}
 }
 
 #ifdef  USE_FULL_ASSERT
